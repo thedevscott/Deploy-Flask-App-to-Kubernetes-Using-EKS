7f6b15b320ecf2bdc475ee2a4fb90f56d0e2c531
Omar Scott <thedevscott@gmail.com>
2020-01-03 15:24:24 -0500

    add tests to pre_build

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def require_jwt(function):
 
 @APP.route('/', methods=['POST', 'GET'])
 def health():
-    return jsonify("Healthy System check ALL GREEN")
+    return jsonify("Healthy")
 
 
 @APP.route('/auth', methods=['POST'])
