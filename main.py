e012d5d2ad173bb488f6bd7c4355563b156ebdc9
Omar Scott <thedevscott@gmail.com>
2020-01-02 23:26:40 -0500

    updated string

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def require_jwt(function):
 
 @APP.route('/', methods=['POST', 'GET'])
 def health():
-    return jsonify("Healthy System")
+    return jsonify("Healthy System check")
 
 
 @APP.route('/auth', methods=['POST'])
